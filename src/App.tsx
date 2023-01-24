50c4f619739a476590e614ea6a05ab608850afab
Denny Setyawan Huda <dennshgit@gmail.com>
2023-01-24 19:59:02 +0700

    chore: install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div className="App">hai</div>;
+  return (
+    <div className="App">
+      <h1 className="md:text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
