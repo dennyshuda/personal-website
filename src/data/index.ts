c7be1bcc9b1fbcd318acdf97980388aaf1d59d2e
Denny Setyawan Huda <dennshgit@gmail.com>
2023-01-27 19:19:54 +0700

    feat: add data skill

---
+++ src/data/index.ts
@@ -0,0 +1,50 @@
+export const Logo = [
+  {
+    id: 1,
+    title: "HTML",
+    imgUrl: "./icons/html.svg",
+    link: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+  },
+  {
+    id: 2,
+    title: "CSS",
+    imgUrl: "./icons/css.svg",
+    link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+  },
+  {
+    id: 3,
+    title: "JavaScript",
+    imgUrl: "./icons/javascript.svg",
+    link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+  },
+  {
+    id: 4,
+    title: "TypeScript",
+    imgUrl: "./icons/typescript.svg",
+    link: "https://www.typescriptlang.org/",
+  },
+  {
+    id: 5,
+    title: "Git",
+    imgUrl: "./icons/git.svg",
+    link: "https://git-scm.com/doc",
+  },
+  {
+    id: 6,
+    title: "React",
+    imgUrl: "./icons/react.svg",
+    link: "https://reactjs.org/",
+  },
+  {
+    id: 7,
+    title: "Tailwind",
+    imgUrl: "./icons/tailwind.svg",
+    link: "https://tailwindcss.com/",
+  },
+  {
+    id: 8,
+    title: "NextJS",
+    imgUrl: "./icons/next.svg",
+    link: "https://nextjs.org/",
+  },
+];
