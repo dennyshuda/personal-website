c69d3f13e1796b7d8982e2ddd5a29da1e398a288
Denny Setyawan Huda <dennshgit@gmail.com>
2023-02-02 21:04:07 +0700

    feat: set dark mode true

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Container } from "./Container";
 
 export function Nav() {
-  const [dark, setDark] = useState(false);
+  const [dark, setDark] = useState(true);
 
   useEffect(() => {
     switch (dark) {
