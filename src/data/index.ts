bf1ba3a0e7b550ddf69be783c826e0ff20e60d8c
Denny Setyawan Huda <dennshgit@gmail.com>
2023-02-06 15:13:23 +0700

    refactor: replace data

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,50 +1,29 @@
-export const Logo = [
+export const ProjectData = [
   {
     id: 1,
-    title: "HTML",
-    imgUrl: "./icons/html.svg",
-    link: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+    title: "Kepo",
+    imgUrl: "./projects/kepo.png",
+    description:
+      "KEPO is a social media website that inspirated by Stack Overflow and Quora.",
+    github: "https://github.com/CrashTeamR/kepo",
+    demo: "https://kepo.dennysh.com/",
   },
   {
     id: 2,
-    title: "CSS",
-    imgUrl: "./icons/css.svg",
-    link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+    title: "Halo Buku",
+    imgUrl: "./projects/halobuku.png",
+    description:
+      "Halo Buku is a website Commerce to buy various books such as novels.",
+    github: "https://github.com/CrashTeamR/kepo",
+    demo: "https://halobuku.dennysh.com/",
   },
   {
     id: 3,
-    title: "JavaScript",
-    imgUrl: "./icons/javascript.svg",
-    link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-  },
-  {
-    id: 4,
-    title: "TypeScript",
-    imgUrl: "./icons/typescript.svg",
-    link: "https://www.typescriptlang.org/",
-  },
-  {
-    id: 5,
-    title: "Git",
-    imgUrl: "./icons/git.svg",
-    link: "https://git-scm.com/doc",
-  },
-  {
-    id: 6,
-    title: "React",
-    imgUrl: "./icons/react.svg",
-    link: "https://reactjs.org/",
-  },
-  {
-    id: 7,
-    title: "Tailwind",
-    imgUrl: "./icons/tailwind.svg",
-    link: "https://tailwindcss.com/",
-  },
-  {
-    id: 8,
-    title: "NextJS",
-    imgUrl: "./icons/next.svg",
-    link: "https://nextjs.org/",
+    title: "Weather App",
+    imgUrl: "./projects/weatherapp.png",
+    description:
+      "Weather App using public API to get details on the specific city around world.",
+    github: "https://github.com/dennyshuda/weather-app",
+    demo: "https://weather-app-denny.vercel.app/",
   },
 ];
