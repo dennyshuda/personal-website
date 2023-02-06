4d5e51dd2ac9eae6e34bb830d6af48350c6b915b
Denny Setyawan Huda <dennshgit@gmail.com>
2023-02-06 15:13:42 +0700

    feat: create project

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { About } from "./components/About";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Nav } from "./components/Nav";
+import { Projects } from "./components/Projects";
 import { Skills } from "./components/Skills";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <About />
       <Skills />
+      <Projects />
       <Footer />
     </div>
   );
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,98 +1,57 @@
 import about from "../assets/about.png";
+import { ProjectData } from "../data";
 import { Container } from "./Container";
 
 export function Projects() {
   return (
-    <div className="dark:bg-[#0C151D] dark:text-white py-5">
+    <div className="dark:bg-darkBackground dark:text-primaryDark text-primaryLight py-5">
       <Container>
-        <div className="mb-10 md:w-3/12 border-b-4 border-primary py-2">
+        <div className="mb-10 md:w-3/12 border-b-4 dark:border-primaryDark border-secondaryLight py-2">
           <h1 className="text-3xl font-bold">
-            My <span className="text-primary">Projects</span>
+            MY{" "}
+            <span className="dark:text-secondaryDark text-secondaryLight">
+              PROJECTS
+            </span>
           </h1>
           <h3>Recent works</h3>
         </div>
-
-        <div className="flex justify-center items-center gap-5 flex-col flex-wrap md:flex-row">
-          <div className="p-6 rounded-lg shadow-lg bg-white max-w-xs">
-            <img
-              className="rounded-lg"
-              src="https://mdbootstrap.com/img/new/standard/nature/184.jpg"
-              alt=""
-            />
-            <div className="mt-2">
-              <h5 className="text-gray-900 text-lg font-medium mb-2">
-                Movie App
-              </h5>
-              <div className="space-x-3">
-                <button
-                  type="button"
-                  className=" inline-block px-6 py-2.5 border-2 border-primary text-primary font-medium text-xs leading-tight rounded-full"
-                >
-                  GitHub
-                </button>
-                <button
-                  type="button"
-                  className=" inline-block px-6 py-2.5 border-2 border-primary text-primary font-medium text-xs leading-tight rounded-full"
-                >
-                  Demo
-                </button>
+        <div className="flex justify-center items-center gap-5 flex-col-reverse flex-wrap md:flex-row-reverse">
+          {ProjectData.map((item) => {
+            return (
+              <div
+                key={item.id}
+                className="p-6 rounded-lg shadow-xl max-w-xs dark:bg-darkBackground bg-white dark:text-primaryDark text-primaryLight"
+              >
+                <img
+                  className="rounded-lg border-4 border-primary dark:border-primaryDark"
+                  src={item.imgUrl}
+                  alt={item.title}
+                />
+                <div className="mt-2 space-y-3">
+                  <h5 className="dark:text-primaryDark text-primary text-lg font-medium mb-2 ">
+                    {item.title}
+                  </h5>
+                  <div>{item.description}</div>
+                  <div className="space-x-3 text-primary dark:text-primaryDark">
+                    <a
+                      href={item.github}
+                      target={"_blank"}
+                      className=" inline-block px-6 py-2.5 border-2 border-primary dark:border-primaryDark font-medium text-xs leading-tight rounded-md"
+                    >
+                      GitHub
+                    </a>
+                    <a
+                      href={item.demo}
+                      target={"_blank"}
+                      className=" inline-block px-6 py-2.5 border-2 border-primary dark:border-primaryDark font-medium text-xs leading-tight rounded-md"
+                    >
+                      Demo
+                    </a>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-
-          <div className="p-6 rounded-lg shadow-lg bg-white max-w-xs">
-            <img
-              className="rounded-lg"
-              src="https://mdbootstrap.com/img/new/standard/nature/184.jpg"
-              alt=""
-            />
-            <div className="mt-2">
-              <h5 className="text-gray-900 text-lg font-medium mb-2">
-                Movie App
-              </h5>
-              <div className="space-x-3">
-                <button
-                  type="button"
-                  className=" inline-block px-6 py-2.5 border-2 border-primary text-primary font-medium text-xs leading-tight rounded-full"
-                >
-                  GitHub
-                </button>
-                <button
-                  type="button"
-                  className=" inline-block px-6 py-2.5 border-2 border-primary text-primary font-medium text-xs leading-tight rounded-full"
-                >
-                  Demo
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <div className="p-6 rounded-lg shadow-lg bg-white max-w-xs">
-            <img
-              className="rounded-lg"
-              src="https://mdbootstrap.com/img/new/standard/nature/184.jpg"
-              alt=""
-            />
-            <div className="mt-2">
-              <h5 className="text-gray-900 text-lg font-medium mb-2">
-                Movie App
-              </h5>
-              <div className="space-x-3">
-                <button
-                  type="button"
-                  className=" inline-block px-6 py-2.5 border-2 border-primary text-primary font-medium text-xs leading-tight rounded-full"
-                >
-                  GitHub
-                </button>
-                <button
-                  type="button"
-                  className=" inline-block px-6 py-2.5 border-2 border-primary text-primary font-medium text-xs leading-tight rounded-full"
-                >
-                  Demo
-                </button>
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </Container>
     </div>
